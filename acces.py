e6b8a9a6e39784d1d093c785a1d78754e2991893
ssocii <152410915+ssocii@users.noreply.github.com>
2024-01-10 22:43:48 +0100

    Add files via upload

---
+++ acces.py
@@ -0,0 +1,46 @@
+import mariadb
+from datetime import datetime
+from mfrc522 import SimpleMFRC522
+lector = SimpleMFRC522()
+import RPi.GPIO as GPIO
+import time
+import os
+def clear():
+    os.system('clear')
+clear()
+
+interruptor = 1
+while interruptor == 1:
+
+    # Establir la connexió amb la DB
+    conex = mariadb.connect(
+        host="localhost",
+        user="auto",
+        password="Autoli.23",
+        database="autolistt"
+    )
+
+    # Crear un cursor per executar les consultes
+    cursor = conex.cursor()
+
+    # Llegira el nombre de xip i el posara dins la variable UID
+    UID, text = lector.read() #Lector.read inicia el lector RFID
+
+    data = datetime.now().date()  # Obtenir la data actual
+    hora = datetime.now().time()  # Obtenir l' hora actual
+
+    # Registra la consulta per a afegir una acces amb el nombre del xip i fecha i hora a la que s'ha passat
+    consulta = "INSERT INTO Acces (UID, Data, Hora) VALUES (%s, %s, %s)"
+    datos = (UID, data, hora)
+
+    try:
+        cursor.execute(consulta, datos) # Executar la consulta
+        print(f"Acces registrat de {UID}")
+        time.sleep(2)
+        clear()
+
+        # Guardar els canvis
+        conex.commit()
+
+    except mariadb.Error as e:
+        print(f"Error al agregar el acceso: {e}")
